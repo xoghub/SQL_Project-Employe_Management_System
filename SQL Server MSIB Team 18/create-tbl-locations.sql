d88411cf04991aeea1f2a59f04d25135350f8d82
xoghub <gunthurbayu1231@gmail.com>
2024-06-17 00:47:29 +0700

    add tbl_locations

---
+++ SQL Server MSIB Team 18/create-tbl-locations.sql
@@ -0,0 +1,12 @@
+USE employee_management_system;
+GO
+
+CREATE TABLE tbl_locations(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	street_address VARCHAR(40),
+	postal_code VARCHAR(12),
+	city VARCHAR(30) NOT NULL,
+	state_province VARCHAR(25),
+	country CHAR(3),
+	CONSTRAINT FK_country_id FOREIGN KEY (country) REFERENCES tbl_country(id) 
+);
