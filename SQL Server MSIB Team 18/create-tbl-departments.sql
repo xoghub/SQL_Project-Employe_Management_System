abccc9c1823fd4b3f078b9d67910dc75cc761ca3
xoghub <gunthurbayu1231@gmail.com>
2024-06-17 00:44:25 +0700

    add tbl-departments

---
+++ SQL Server MSIB Team 18/create-tbl-departments.sql
@@ -0,0 +1,9 @@
+USE employee_management_system;
+GO
+
+CREATE TABLE tbl_departments(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	name VARCHAR(30) NOT NULL,
+	location_id INT,
+	CONSTRAINT FK_location_id FOREIGN KEY (location_id) REFERENCES tbl_location(id)
+);
