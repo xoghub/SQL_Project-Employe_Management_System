7ccf381d5575eb91e43887a1ae04648bb1655dbf
xoghub <gunthurbayu1231@gmail.com>
2024-06-17 11:35:58 +0700

    correct tbl-departments

--- SQL Server MSIB Team 18/create-tbl-departments.sql
+++ SQL Server MSIB Team 18/create-tbl-departments.sql
@@ -5,5 +5,5 @@ CREATE TABLE tbl_departments(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	name VARCHAR(30) NOT NULL,
 	location_id INT,
-	CONSTRAINT FK_location_id FOREIGN KEY (location_id) REFERENCES tbl_location(id)
+	CONSTRAINT FK_location_id FOREIGN KEY (location_id) REFERENCES tbl_locations(id)
 );
