b539efcb0aa88d87968de92fa8877729da837108
xoghub <gunthurbayu1231@gmail.com>
2024-06-17 12:13:37 +0700

    correct tbl-account-role

--- SQL Server MSIB Team 18/create-tbl-acccount-role.sql
+++ SQL Server MSIB Team 18/create-tbl-acccount-role.sql
@@ -5,6 +5,6 @@ CREATE TABLE tbl_account_roles(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	account_id INT,
 	role_id INT,
-	CONSTRAINT FK_account_id FOREIGN KEY (account_id) REFERENCES tbl_account(id),
-	CONSTRAINT FK_role_id FOREIGN KEY (role_id) REFERENCES tbl_roles(id)
+	CONSTRAINT FK_account_id FOREIGN KEY (account_id) REFERENCES tbl_accounts(id),
+	CONSTRAINT FK_role_account_id FOREIGN KEY (role_id) REFERENCES tbl_roles(id)
 );
